ef012abfa829c94133b928429c0d37b1380d0c78
Atif Ali <atif@monotote.com>
2021-09-08 16:25:10 +0500

    Uptil Nav and Banner components

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import requests from './request';
 
 function App() {
   return (
-    <div className="App">
       <div className="app">
-        <h1>Hey world</h1>
-          <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+        {/* Nav */}
+        { /* Banner  */}
+          <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
           <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+          <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+          <Row title="Action movies" fetchUrl={requests.fetchActionMovies}/>
+          <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies}/>
+          <Row title="Horror movies" fetchUrl={requests.fetchHorrorMovies}/>
+          <Row title="Romance movies" fetchUrl={requests.fetchRomanticMovies}/>
+          <Row title="Documentries" fetchUrl={requests.fetchDocumentaries}/>
       </div>
-    </div>
   );
 }
 
